9029c3888c6e6a7210f6c1808b65452ed3b2d165
Developer SRG <shivamgarade05@gmail.com>
2026-01-08 08:37:49 +0000

    added dog model to websie and added meshmatcapmaterial to it and fixed its position properly as per need and also added normalMap to it

--- wolf project/src/App.jsx
+++ wolf project/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <Canvas>
-    <Dog />
+      <Dog />
     </Canvas>
   )
 }
--- wolf project/src/components/Dog.jsx
+++ wolf project/src/components/Dog.jsx
@@ -1,20 +1,37 @@
-import { Canvas, useThree } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { useThree } from '@react-three/fiber'
+import { OrbitControls, useGLTF,useTexture } from '@react-three/drei'
+import * as THREE from "three"
 
 const Dog = () => {
+
+    const { scene } = useGLTF("/models/dog.drc.glb")
+
     useThree(({ camera, scene, gl }) => {
-        console.log(camera.position)
+        camera.position.z = 0.75
+    })
+
+    const textures = useTexture({
+        normalMap:"/dog_normals.jpg"
+    })
+
+    textures.normalMap.flipY = false
+
+    // har ek object ke liye ek ek baar chalege
+    scene.traverse((child)=>{
+        if(child.name.includes("DOG")){
+            child.material = new THREE.MeshMatcapMaterial({
+                normalMap:textures.normalMap
+            })
+        }
     })
     return (
         <>
-            <mesh>
-                <meshBasicMaterial color={0x00FF00} />
-                <boxGeometry args={[1, 1, 1]} />
-            </mesh>
+            <primitive object={scene} position={[0.25, -0.6, 0]} rotation={[0, Math.PI / 3.55, 0]} />
+            <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
             <OrbitControls />
-            {/* stopped at 1:07:52 */}
         </>
     )
 }
 
 export default Dog
+// stopped at 1:50:44
