266a71d5d21b8d1cf8ed49da18bfbcdb81a27dba
Shivam Garade <shivamgarade05@gmail.com>
2026-01-17 22:21:26 +0530

    Writing the first three.js code

--- main.js
+++ main.js
@@ -5,4 +5,12 @@ const scene = new THREE.Scene()
 
 // to see world we need camera
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
-// Create a Three.js scene with a spinning green cube and a directional light
+// no unit in three js
+
+// add camera to scene
+scene.add(camera)
+
+// Mesh - (Shape & Material)
+// shape
+
+
